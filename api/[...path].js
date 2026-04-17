8f8849cfbdd2350b215bedcfb22e74ca90b4e746
manojeluri <manojeluri21@gmail.com>
2026-04-18 01:37:51 +0530

    Add debug logging to Vercel function

--- api/[...path].js
+++ api/[...path].js
@@ -1,1 +1,7 @@
-module.exports = require('../backend/server');
+const app = require('../backend/server');
+
+module.exports = (req, res) => {
+  // Debug: log what URL Vercel passes to Express
+  console.log('[vercel-fn] method:', req.method, 'url:', req.url);
+  return app(req, res);
+};
